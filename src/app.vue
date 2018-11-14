c381454a4492ce75d54d70838e1b69d4da26201a
e <ztshinemoi@163.com>
2018-11-14 10:31:42 +0800

    更新

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        hello, world
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+}
+</script>
+
+<style scoped>
+    #app{
+        color: red;
+    }
+</style>
+
+
